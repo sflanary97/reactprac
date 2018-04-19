3e2f4cb7974f54c4e29d9bd51cc037f2feac3c43
sflanary97 <sflanary97@gmail.com>
2018-04-19 15:22:43 -0700

    Add modules

---
+++ scoreboard/webpack.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  entry: '.app.js',
+
+  output:{
+    //path: 'build',
+    filename: 'bundle.js'
+  },
+
+  module:{
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/.
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
